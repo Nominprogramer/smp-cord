24206a33268cc73a1eb97a2b48096042bb4a4f96
IronLung127 <IronLung127@gmail.com>
2022-02-28 11:54:41 +0530

    added start/stop functions

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import discord
 
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+    # noinspection PyMethodMayBeStatic
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
-        if (message.content.startswith("$", 0)):
-            print("COmmannd")
-            
+        if message.content.startswith("$", 0):
+            print("Command")
+
+            if message.content == "$start":
+                print("Attempting to start server")
+
+            elif message.content == "$stop":
+                print("Attempting to stop server")
+
 
 client = MyClient()
 client.run('OTQwMTAzOTkzMzQwODY2NjUx.YgCiEg._H0L3cFSYsmDhax4uXndcYSDLWg')
