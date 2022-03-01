6d28d8ec2285bfafe063f28a166fb849dcc928cc
IronLung127 <IronLung127@gmail.com>
2022-03-01 16:44:37 +0530

    Fixed and improved user logging

--- main.py
+++ main.py
@@ -1,66 +1,69 @@
 import discord
 import minestat
 import os.path
-#import logging
 
-#logger = logging.getLogger("discord")
-#logger.setLevel(logging.DEBUG)
-#handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-#handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-#logger.addHandler(handler)
 
 def ServerStatus() -> object:
     ms = minestat.MineStat("64.227.162.110", 25565)
     # print(ms.online)
+    # noinspection PySimplifyBooleanCheck
     if ms.online == True:
         return True
     else:
         return False
 
-def log(file,msg):
-    if os.path.exists(file) == True :
-        logfile = open(file,"w")
-    else :
-        logfile = open(file , "x")
-        logfile = open(file , "w")
+
+def log(file, msg):
+    if os.path.exists(file):
+        logfile = open(file, "a")
+    else:
+        # noinspection PyUnusedLocal
+        logfile = open(file, "x")
+        logfile = open(file, "a")
 
     logfile.write(msg)
+    logfile.close()  # good practice
 
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-        print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
+    sender = str(message.author)
+    # logger.debug(sender)
+    # logger.debug(message.content)
+    # log('discord.log', sender)
+    # log('discord.log', message.content)
 
-        sender = str(message.author)
-       #logger.debug(sender)
-        #logger.debug(message.content)
-        log('discord.log',sender)
-        log('discord.log',message.content)
+    if message.author == client.user:
+        return
 
-        if message.author == client.user:
-                return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-        if message.content.startswith('$hello'):
-                await message.channel.send('Hello!')
+    if message.content.startswith('$start'):
+        await message.channel.send("Attempting to start server")
+        if ServerStatus():
+            await message.channel.send("Server is already online :D")
+        else:
+            await message.channel.send("Starting Server on the request of" + sender + '\n')
+            print("Starting Server on the request of" + sender)
+            log("discord.log", sender + ', ')
 
-        if message.content.startswith('$start'):
-                await message.channel.send("Attempting to start server")
-                if ServerStatus():
-                    await message.channel.send("Server is already online :D")
-                else:
-                    await message.channel.send("Starting Server on the request of" + sender)
+    elif message.content == "$stop":
+        sender = message.author
+        await message.channel.send("Attempting to stop server")
+        if not ServerStatus():
+            await message.channel.send("Server is already Offline")
+        else:
+            print("Stopping Server on the request of" + sender)
+            log("discord.log", sender + ', ')
 
-        elif message.content == "$stop":
-             sender = message.author
-             await message.channel.send("Attempting to stop server")
-             if not ServerStatus():
-                 await message.channel.send("Server is already Offline")
-             else:
-                 print("Stopping Server on the request of" + sender)
 
-client.run('')
+client.run('OTQwMTAzOTkzMzQwODY2NjUx.YgCiEg._H0L3cFSYsmDhax4uXndcYSDLWg')
